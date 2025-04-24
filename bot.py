c7e03653854f6b041e9677eb1ded96ae62a68a7c
Vangor1 <62812500+Vangor1@users.noreply.github.com>
2025-04-24 16:54:31 +0300

    0.0061

--- bot.py
+++ bot.py
@@ -5,19 +5,31 @@ from handlers.schedule import (schedule, send_reminder)
 from handlers.list import list_reminders
 from handlers.cancel import cancel
 import db
-from telegram.ext import (CommandHandler, ApplicationBuilder)
+from telegram import BotCommand
+from telegram.ext import (CommandHandler, ApplicationBuilder,
+                          Application)
 from config import BOT_TOKEN
 
 logging.basicConfig(
     format='%(asctime)s -%(name)s - %(levelname)s - %(message)s', 
     level=logging.INFO)
 logger=logging.getLogger(__name__)
-
+async def set_bot_commands(app:Application):
+    """
+    Установка всплывающих подсказок команд
+    """
+    commands= [
+        BotCommand('start', 'Запустить бота'),
+        BotCommand('schedule', 'Установить напоминание'),
+        BotCommand('list','Показать все напоминания'),
+        BotCommand('cancel','Отменить напоминание по ID')
+    ]
+    await app.bot.set_my_commands(commands)
 def main():
     #Инициализация бд
     db.init_db()
     #Псотройка приложения и регистрация команд
-    application = ApplicationBuilder().token(BOT_TOKEN).build()
+    application = ApplicationBuilder().token(BOT_TOKEN).post_init(set_bot_commands).build()
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("schedule", schedule))
     application.add_handler(CommandHandler("list", list_reminders))
